e5e5de42057d7f7dc4ca336ae96ceff6ba950d6b
Folosman <63555425+Folosman@users.noreply.github.com>
2022-10-19 23:59:11 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+
+    w.setWindowIcon(QIcon("../SimpleAudioPlayer/Icons/Diablo"));
     w.resize(1280, 800);
     w.show();
     return a.exec();
