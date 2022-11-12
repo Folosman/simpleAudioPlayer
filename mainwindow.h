814d52c226ea9f9c141983060799f2b656d197d2
Folosman <63555425+Folosman@users.noreply.github.com>
2022-11-13 01:22:30 +0500

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,8 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "editplaylist.h"
+
 #include <QMainWindow>
 #include <QStandardItemModel>
 #include <QMediaPlayer>
@@ -19,13 +21,19 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_addBtn_clicked();
-
+    void addBtn();
+    void addNewPlaylist();
+    void clearBtn();
+    void editBtn();
 private:
     Ui::MainWindow *ui;
+
+    EditPlaylist m_editPlaylist;
+
     QMediaPlayer *m_player;
     QMediaPlaylist *m_playlist;
     QStandardItemModel *m_playlistModel;
+    QStandardItemModel *m_playlistListModel;
 };
 
 #endif
